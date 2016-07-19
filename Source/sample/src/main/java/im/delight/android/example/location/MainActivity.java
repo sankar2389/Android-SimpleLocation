6b66392d020782744767830d2f352a9e3038332b
sankar2389 <sankar2389@gmail.com>
2016-07-19 18:54:16 +0530

    Get Address (Street, City, Country) from Lat & Lon
    	String addressText = String.format(
    						"%s, %s, %s",
    						// If there's a street address, add it
    						address.getMaxAddressLineIndex() > 0 ? address
    								.getAddressLine(0) : "",
    						// Locality is usually a city
    						address.getLocality(),
    						// The country of the address
    						address.getCountryName());

--- Source/sample/src/main/java/im/delight/android/example/location/MainActivity.java
+++ Source/sample/src/main/java/im/delight/android/example/location/MainActivity.java
@@ -10,6 +10,8 @@ import im.delight.android.location.SimpleLocation;
 public class MainActivity extends Activity {
 
     private SimpleLocation mLocation;
+    String address = "";
+    double latitude, longitude;
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
@@ -32,16 +34,65 @@ public class MainActivity extends Activity {
 
             @Override
             public void onClick(View v) {
-                final double latitude = mLocation.getLatitude();
-                final double longitude = mLocation.getLongitude();
+                latitude = mLocation.getLatitude();
+                longitude = mLocation.getLongitude();
+		address = getLocationAddress();
 
                 Toast.makeText(MainActivity.this, "Latitude: "+latitude, Toast.LENGTH_SHORT).show();
                 Toast.makeText(MainActivity.this, "Longitude: "+longitude, Toast.LENGTH_SHORT).show();
+				Toast.makeText(MainActivity.this, "Address: "+address, Toast.LENGTH_LONG).show();
             }
 
         });
 	}
 
+public String getLocationAddress() {
+
+			Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+			// Get the current location from the input parameter list
+			// Create a list to contain the result address
+			List<Address> addresses = null;
+			try {
+				/*
+				 * Return 1 address.
+				 */
+				addresses = geocoder.getFromLocation(latitude, longitude, 1);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+				return ("IO Exception trying to get address:" + e1);
+			} catch (IllegalArgumentException e2) {
+				// Error message to post in the log
+				String errorString = "Illegal arguments "
+						+ Double.toString(latitude) + " , "
+						+ Double.toString(longitude)
+						+ " passed to address service";
+				e2.printStackTrace();
+				return errorString;
+			}
+			// If the reverse geocode returned an address
+			if (addresses != null && addresses.size() > 0) {
+				// Get the first address
+				Address address = addresses.get(0);
+				/*
+				 * Format the first line of address (if available), city, and
+				 * country name.
+				 */
+				String addressText = String.format(
+						"%s, %s, %s",
+						// If there's a street address, add it
+						address.getMaxAddressLineIndex() > 0 ? address
+								.getAddressLine(0) : "",
+						// Locality is usually a city
+						address.getLocality(),
+						// The country of the address
+						address.getCountryName());
+				// Return the text
+				return addressText;
+			} else {
+				return "No address found by the service: Note to the developers, If no address is found by google itself, there is nothing you can do about it.";
+			}
+	}
+	
     @Override
     protected void onResume() {
         super.onResume();
